8797edbada6c009ab9b4117a865013629f561551
Victor Bianchi <vbianchi90@gmail.com>
2018-02-19 12:02:24 -0500

    completed exercise 01

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'ask', to: 'questions#ask'
+
+  post 'answer', to: 'questions#answer'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 end
